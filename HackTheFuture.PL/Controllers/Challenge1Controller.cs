82909ac90c40a2fc58e5c4e894ac41095f429a01
Otto Hein <33087686+OttoHein@users.noreply.github.com>
2018-11-29 10:15:34 +0100

    CHanged structure

---
+++ HackTheFuture.PL/Controllers/Challenge1Controller.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HackTheFuture.PL.Controllers
+{
+    public class Challenge1Controller : Controller
+    {
+        // GET: Challenge1
+        public ActionResult GetChallenge()
+        {
+            string resultString = "";
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://htf2018.azurewebsites.net/");
+                //HTTP GET
+                var responseTask = client.GetAsync("Challenges");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsStreamAsync();
+                    readTask.Wait();
+
+                    StreamReader reader = new StreamReader(readTask.Result);
+                    resultString = reader.ReadToEnd();
+                }
+                else //web api sent error response 
+                {
+                    //log response status here.
+                    ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+                }
+            }
+
+            ViewBag.resultString = resultString;
+
+            return View();
+        }
+    }
+}
--- HackTheFuture.PL/Controllers/HomeController.cs
+++ HackTheFuture.PL/Controllers/HomeController.cs
@@ -12,32 +12,6 @@ namespace HackTheFuture.PL.Controllers
     {
         public ActionResult Index()
         {
-            string resultString = "";
-
-            using (var client = new HttpClient())
-            {
-                client.BaseAddress = new Uri("http://htf2018.azurewebsites.net/");
-                //HTTP GET
-                var responseTask = client.GetAsync("Challenges");
-                responseTask.Wait();
-
-                var result = responseTask.Result;
-                if (result.IsSuccessStatusCode)
-                {
-                    var readTask = result.Content.ReadAsStreamAsync();
-                    readTask.Wait();
-
-                    StreamReader reader = new StreamReader(readTask.Result);
-                    resultString = reader.ReadToEnd();
-                }
-                else //web api sent error response 
-                {
-                    //log response status here.
-                    ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
-                }
-            }
-
-            ViewBag.resultString = resultString;
 
             return View();
         }
