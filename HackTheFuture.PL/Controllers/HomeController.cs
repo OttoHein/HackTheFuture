519e761e417258be31481b5b0ea2aab40af37c2b
Otto Hein <33087686+OttoHein@users.noreply.github.com>
2018-11-29 09:54:19 +0100

    Init

---
+++ HackTheFuture.PL/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HackTheFuture.PL.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            string resultString = "";
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://htf2018.azurewebsites.net/");
+                //HTTP GET
+                var responseTask = client.GetAsync("Challenges");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsStreamAsync();
+                    readTask.Wait();
+
+                    resultString = readTask.Result.ToString();
+                }
+                else //web api sent error response 
+                {
+                    //log response status here.
+                    ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+                }
+            }
+
+            ViewBag.resultString = resultString;
+
+            return View();
+        }
+    }
+}
